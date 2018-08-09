5db7b5c2074d81422f4874f863c99f06d60bf1ab
Joerg Helwig <helwigjoerg@gmail.com>
2018-08-09 17:22:21 +0200

    final function

--- spark_nasa_log_parser.scala
+++ spark_nasa_log_parser.scala
@@ -1,54 +1,62 @@
+import org.apache.spark.sql.DataFrame
+
 
 case class LogRecord( host: String, timeStamp: String, url:String,httpCode:Int)
 
 val PATTERN = """^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)(.*)" (\d{3}) (\S+)""".r
 
-def parseLogLine(log: String):
-	LogRecord = {
-		val res = PATTERN.findFirstMatchIn(log) 
-		if (res.isEmpty)
-		{
-			println("Rejected Log Line: " + log)
-			LogRecord("Empty", "", "",  -1 )
-		}
-		else 
-		{
-			val m = res.get
-			LogRecord(m.group(1), m.group(4),m.group(6), m.group(8).toInt)
-		}
-	}
+val logFile = sc.textFile("/data/spark/project/NASA_access_log_Aug95.gz")
+val accessLog = logFile.map(parseLogLine)
+val accessDf = accessLog.toDF()
+accessDf.printSchema
+val inputData=prepareData(accessDf) 
+println("topLogRecord")
+topLogRecord(inputData).show
+println("highTrafficWeefDay")
+highTrafficWeefDay(inputData).show
+println("lowTrafficWeefDay")
+lowTrafficWeefDay(inputData).show
+println("highTrafficHour")
+highTrafficHour(inputData).show
+println("lowTrafficHour")
+lowTrafficHour(inputData).show
 
-def parseLogLine1(log: String) : 
+
+
+def parseLogLine(log: String) : 
  LogRecord = {  
    log match {  case PATTERN(host, group2, group3,timeStamp,group5,url,group7,httpCode,group8) => LogRecord(s"$host",s"$timeStamp",s"$url", s"$httpCode".toInt) 
 		case _ => LogRecord("Empty", "", "",  -1 )}}
 
-def setUpdata (){
-val newDf =accessDf.withColumn("Date",unix_timestamp(accessDf.col("timeStamp"), "dd/MMM/yyyy:HH:mm:ss").cast("timestamp")).withColumn("unix_ts" , unix_timestamp($"Date") ).withColumn("year", year(col("Date"))).withColumn("m
+def prepareData (input: DataFrame): DataFrame = {
+ input.select($"*").filter($"host" =!= "Empty").withColumn("Date",unix_timestamp(accessDf.col("timeStamp"), "dd/MMM/yyyy:HH:mm:ss").cast("timestamp")).withColumn("unix_ts" , unix_timestamp($"Date") ).withColumn("year", year(col("Date"))).withColumn("m
 onth", month(col("Date"))).withColumn("day", dayofmonth(col("Date"))).withColumn("hour", hour(col("Date"))).withColumn("weekday",from_unixtime(unix_timestamp($"Date", "MM/dd/yyyy"), "EEEEE"))	
 }
 
-def topLogRecord (){
-	accessDf.select($"url").filter(upper($"url").like("%HTML%")).groupBy($"url").agg(count("*").alias("cnt")).orderBy(desc("cnt")).limit(10).show()
+def topLogRecord(input: DataFrame): DataFrame = {
+	input.select($"url").filter(upper($"url").like("%HTML%")).groupBy($"url").agg(count("*").alias("cnt")).orderBy(desc("cnt")).limit(10)
     
 }
- 
 
-val logFile = sc.textFile("/data/spark/project/NASA_access_log_Aug95.gz")
-val accessLog = logFile.map(parseLogLine1)
-val accessDf = accessLog.toDF()
-accessDf.printSchema
-accessDf.createOrReplaceTempView("nasalog")
-val output = spark.sql("select * from nasalog")
-output.createOrReplaceTempView("nasa_log")
-spark.sql("cache TABLE nasa_log")
+ def highTrafficWeefDay (input: DataFrame): DataFrame = {
+	 input.select($"weekday").groupBy($"weekday").agg(count("*").alias("count_weekday")).orderBy(desc("count_weekday")).limit(5)
+	 
+ }
 
-spark.sql("select url,count(*) as req_cnt from nasa_log where upper(url) like '%HTML%' group by url order by req_cnt desc LIMIT 10").show
+ def lowTrafficWeefDay (input: DataFrame): DataFrame = {
+	 input.select($"weekday").groupBy($"weekday").where($"weekday" not null).agg(count("*").alias("count_weekday")).orderBy(asc("count_weekday")).limit(5)
+	 
+ }
 
-spark.sql("select host,count(*) as req_cnt from nasa_log group by host order by req_cnt desc LIMIT 5").show
+def highTrafficHour (input: DataFrame): DataFrame = {
+	 input.select($"hour").groupBy($"hour").agg(count("*").alias("count_hour")).orderBy(desc("count_hour")).limit(5)
+	 
+ }
 
-spark.sql("select substr(timeStamp,1,14) as timeFrame,count(*) as req_cnt from nasa_log group by substr(timeStamp,1,14) order by req_cnt desc LIMIT 5").show
+def lowTrafficHour (input: DataFrame): DataFrame = {
+	 input.select($"hour").groupBy($"hour").agg(count("*").alias("count_hour")).orderBy(asc("count_hour")).limit(5)
+	 
+ }
 
-spark.sql("select substr(timeStamp,1,14) as timeFrame,count(*) as req_cnt from nasa_log group by substr(timeStamp,1,14) order by req_cnt  LIMIT 5").show
 
-spark.sql("select httpCode,count(*) as req_cnt from nasa_log group by httpCode ").show
+
